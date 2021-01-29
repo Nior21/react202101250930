94c542f6b1b5b5d013e155f3ff038d99089231a1
Nior21 <lhall@ya.ru>
2021-01-29 12:55:27 +0300

    - Исправил ошибку в <Header />, не отображался desc (имя descr в IDE подчеркивается как нежелательное) - Изменил порядок изображений для большей лакончности. К сожалению Пикачу не попадает в отображаемую часть изображения на небольших экранах, зато цвета блоков сочетаются лучше. - Добавил тестовые заголовки и текст в блоки <Layout /> для оценки корректности работы стилей

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Header from "./components/Header";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
 
-import firstImg from './accert/bg3.jpg';
-import secondImg from './accert/bg2.jpg';
+import firstImg from './accert/bg1.jpg';
+import secondImg from './accert/bg3.jpg';
 
 const App = () => {
   return (
       <>
         <Header
-            title="This is new Title"
+            title="This is a new Title"
             desc="This is Description!"
         />
-        <Layout urlBg={firstImg} />
-        <Layout colorBg="gray" />
-        <Layout urlBg={secondImg} />
+        <Layout urlBg={firstImg} title="This is the first Layout" desc="This is Description" />
+        <Layout colorBg="gray" title="This is the second Layout" desc="This is Description" />
+        <Layout urlBg={secondImg} title="This is the third Layout" desc="This is Description" />
         <Footer />
       </>);
 }
