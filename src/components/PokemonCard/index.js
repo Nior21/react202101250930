cb71d15cfb4e7ea6f23fabe3ccb7da447d58879d
Nior21 <lhall@ya.ru>
2021-01-31 17:25:26 +0300

    * Заменён класс стиля на s[type], это исправляет фон карточек

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -14,7 +14,7 @@ const PokemonCard = ({name, img, id, type, values}) => {
             <div className={`${s.pokemonCard} ${isActive ? s.active : ''}`}>
                 <div className={s.cardFront}>
                     <div className={`${s.wrap} ${s.front}`}>
-                        <div className={s.pokemon}>
+                        <div className={`${s.pokemon} ${s[type]}`}>
                             <div className={s.values}>
                                 <div className={`${s.count} ${s.top}`}>{values.top}</div>
                                 <div className={`${s.count} ${s.right}`}>{values.right}</div>
@@ -27,7 +27,7 @@ const PokemonCard = ({name, img, id, type, values}) => {
                             <div className={s.info}>
                                 <span className={s.number}>#{id}</span>
                                 <h3 className={s.name}>{name}</h3>
-                                <small className={`s.${type}`}>
+                                <small className={s[type]}>
                                     Type: <span>{type}</span>
                                 </small>
                             </div>
