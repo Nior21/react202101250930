aa55cadb9e2860db8c785a977c852eff7035ed22
Nior21 <lhall@ya.ru>
2021-02-07 00:43:48 +0300

    * Подключена база Firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useRouteMatch, Route, Switch, Redirect } from "react-router-dom"
 import cn from "classnames";
+import firebase from "firebase";
 
 import HomePage from "./routes/Home";
 import GamePage from "./routes/Game";
@@ -10,6 +11,24 @@ import ContactPage from "./routes/Contact";
 import NotFoundPage from "./routes/NotFound";
 
 import s from "./style.module.css";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCpzO1F28gHp5nclrC1aoPJlzmTSasKT-o",
+    authDomain: "pokemon-game-e1680.firebaseapp.com",
+    databaseURL: "https://pokemon-game-e1680-default-rtdb.firebaseio.com",
+    projectId: "pokemon-game-e1680",
+    storageBucket: "pokemon-game-e1680.appspot.com",
+    messagingSenderId: "410074435445",
+    appId: "1:410074435445:web:719c2b2623a1156cba0d3c"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+database.ref('pokemons').once('value', (snapshot) => {
+    console.log('snapshot', snapshot.val());
+})
 
 const App = () => {
     const match = useRouteMatch('/');
