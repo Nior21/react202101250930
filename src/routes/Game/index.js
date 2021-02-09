d0409f186db3f29d2d7cdcc989d0f76f9eb7bf88
Nior21 <lhall@ya.ru>
2021-02-09 11:24:24 +0300

    * Реализована функция добавления новой карточки на игровое поле.

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -5,12 +5,13 @@ import Layout from "../../components/Layout";
 import database from "../../service/firebase";
 
 import s from "./style.module.css";
+import {generateUniqueID} from "web-vitals/dist/lib/generateUniqueID";
 
 const GamePage = () => {
     const [pokemons, setPokemons] = useState({});
 
     useEffect(() => {
-        database.ref ( "pokemons" ).once ("value", (snapshot) => {
+        database.ref ( "pokemons" ).once("value", (snapshot) => {
             setPokemons (snapshot.val ())
         })
     }, [])
@@ -34,11 +35,52 @@ const GamePage = () => {
         })
     }
 
+    const addPokemon = () => {
+        const newKey = database.ref().child("pokemons").push().key;
+
+
+        const updates = {};
+        updates["/pokemons/" + newKey] =
+            {
+                abilities: ["keen-eye", "tangled-feet", "big-pecks"],
+                base_experience: 122,
+                height: 11,
+                id: 25,
+                img:
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
+                name: "pidgeotto",
+                stats: {
+                    attack: 60,
+                    defense: 55,
+                    hp: 63,
+                    "special-attack": 50,
+                    "special-defense": 50,
+                    speed: 71,
+                },
+                type: "flying",
+                values: {
+                    bottom: 7,
+                    left: 5,
+                    right: 2,
+                    top: "A",
+                },
+            };
+
+        return database.ref().update(updates);
+    };
+
+
+
     return (
         <>
             <Layout id="game"
                     title="Game"
             >
+                <div>
+                    <button onClick={addPokemon} className="btn btn-secondary mr-2">
+                        Add Pokemon
+                    </button>
+                </div>
                 <div className={s.flex}>
                     {
                         Object.entries(pokemons).map((
