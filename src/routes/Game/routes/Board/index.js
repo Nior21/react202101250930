1710a1f1ea0233c1fcb155848a836d5746890377
Nior21 <lhall@ya.ru>
2021-02-12 20:19:14 +0300

    * Получаем сетку значений board * Возвращаем позицию при щелчках по board * Реализована загрузка случайных карточек c адреса "https://reactmarathon-api.netlify.app/api/" * Настроено отображение карт противника (правки в CSS) * Выделен в отдельную компоненту код для перечисления карточек игрока * Колоды карт игрока 1 и игрока 2 переключены на использование выделенной компоненты * Добавлен стиль и реакция на клик по картам в колодах страницы Board * Настроен механизм передачи данных о выбранной карты на игровое поле в момент клика по ячейке игрового поля.

--- src/routes/Game/routes/Board/index.js
+++ src/routes/Game/routes/Board/index.js
@@ -1,47 +1,70 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {useHistory} from "react-router-dom"
 import {PokemonContext} from "../../../../context/pokemonContext";
 import PokemonCard from "../../../../components/PokemonCard";
+import PlayerBoard from "./component/PlayerBoard";
 
 import s from './style.module.css';
 
 const BoardPage = () => {
+    const [board, setBoard] = useState ( [] )
+    const [player2, setPlayer2] = useState ( [] )
+    const [choiceCard, setChoiceCard] = useState ( null )
     const {pokemons} = useContext ( PokemonContext )
-    const history = useHistory()
+    const history = useHistory ()
 
-    if (Object.keys(pokemons).length === 0) {
-        history.replace("/game")
+    console.log ( '####: board', board, player2 )
+
+    // if (Object.keys(pokemons).length === 0) {
+    //     history.replace("/game")
+    // }
+
+    useEffect ( async () => {
+        const boardResponse = await fetch ( 'https://reactmarathon-api.netlify.app/api/board' )
+        const boardRequest = await boardResponse.json ()
+
+        setBoard ( boardRequest.data )
+
+        const player2Response = await fetch ( 'https://reactmarathon-api.netlify.app/api/create-player' )
+        const player2Request = await player2Response.json ()
+
+        setPlayer2 ( player2Request.data )
+    }, [] )
+
+    function handleClickBoardPlate(position) {
+        console.log ( '###: position', position )
+        console.log ( '###: choiceCard', choiceCard )
     }
 
     return (
         <div className={ s.root }>
             <div className={ s.playerOne }>
+                <PlayerBoard
+                    cards={ Object.values ( pokemons ) }
+                    onClickCard={(card) => setChoiceCard(card)}
+                />
+            </div>
+            <div className={ s.board }>
                 {
-                    Object.values ( pokemons ).map ( ({id, name, img, type, values}) => (
-                        <PokemonCard
-                            key={ id }
-                            id={ id }
-                            name={ name }
-                            img={ img }
-                            type={ type }
-                            values={ values }
-                            className={ s.card }
-                            minimize
-                            isActive
-                        />
+                    board.map ( item => (
+                        <div
+                            key={ item.position }
+                            className={ s.boardPlate }
+                            onClick={ () => !item.card && handleClickBoardPlate ( item.position ) }
+                        >
+                            {
+                                item.card && <PokemonCard { ...item } minimize/>
+                            }
+                        </div>
                     ) )
                 }
             </div>
-            <div className={ s.board }>
-                <div className={ s.boardPlate }>1</div>
-                <div className={ s.boardPlate }>2</div>
-                <div className={ s.boardPlate }>3</div>
-                <div className={ s.boardPlate }>4</div>
-                <div className={ s.boardPlate }>5</div>
-                <div className={ s.boardPlate }>6</div>
-                <div className={ s.boardPlate }>7</div>
-                <div className={ s.boardPlate }>8</div>
-                <div className={ s.boardPlate }>9</div>
+
+            <div className={ s.playerTwo }>
+                <PlayerBoard
+                    cards={ player2 }
+                    onClickCard={(card) => setChoiceCard(card)}
+                />
             </div>
         </div>
     );
